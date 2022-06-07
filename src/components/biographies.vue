5ecee914f4b1c3b0c08ef9ebb0ba6257c511b453
Sahil-Nandal <sahil52001nandal@gmail.com>
2022-06-07 15:39:44 +0530

    2 files modified, 2 added

---
+++ src/components/biographies.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Biographies</h1>
+  <h3>This is the about page</h3>
+</template>
+
+<script>
+export default {
+  name: 'biographies',
+};
+</script>
---
+++ src/components/novels.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Novels</h1>
+  <h3>This is the about page</h3>
+</template>
+
+<script>
+export default {
+  name: 'novels',
+};
+</script>
